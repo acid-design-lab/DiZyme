d97248155ba578e2c3ad31077833f2549d8c5dae
julia <razlivina@scamt-itmo.ru>
2023-09-11 15:25:29 +0300

    files 2.0

--- processing/functions.py
+++ processing/functions.py
@@ -59,17 +59,15 @@ def intindex(formula):
         return formula
 
 import csv
+import csv
 def Redox(element, os1, os2):
-    with open(r'C:\Users\julia\PycharmProjects\NZymes\redox1.csv', 'r') as f:
+    with open('redox1.csv', 'r') as f:
         reader = csv.reader(f)
-        next(reader) # skip the header row
+        next(reader)
         for row in reader:
-            try:
-                el1, el2, potential = row
-                if el1 == f"{element}[{os1}]" and el2 == f"{element}[{os2}]":
-                    return float(potential)
-            except ValueError:
-                print(f"Error on line {row}")
+            el1, el2, potential = row
+            if el1 == f"{element}[{os1}]" and el2 == f"{element}[{os2}]":
+                return float(potential)
     return None
 
 def OS(comp):
@@ -101,3 +99,85 @@ def elfromcomp(comp):
         element_var_name = symbol
         elements[element_var_name] = float(index)
     return elements
+
+def separate_formulas(df, column):
+    formulas = []
+    components = []
+
+    for entry in column:
+        if not isinstance(entry, str):
+            formulas.append(None)
+            components.append(None)
+            continue
+        match = re.split(r'\s*\+\s*', entry)
+        if match and len(match) == 2:
+            formulas.append(match[0])
+            components.append(match[1])
+        else:
+            formulas.append(match[0])
+            components.append(None)
+
+    df['sub1'] = formulas
+    df['sub2'] = components
+    return df
+def split(strings):
+    excluded_strings = ['1,3,5-benzenetricarboxylate', '3,5-Dimethylpyridine']
+    result = []
+    for string in strings:
+        if string in excluded_strings:
+            result.append(string)
+            continue
+        if ',' in string:
+            parts = string.split(',')
+            result.extend(parts)
+        else:
+            result.append(string)
+    return result
+
+def monomer(string_list):
+    substrings = []
+    for string in string_list:
+        if string == "Tetrakis(4-carboxyphenyl)porphine":
+            substrings.append(string)
+            continue
+        match = re.search(r'\((.*?)\)', string)
+        if match:
+            substrings.append(match.group(1))
+        else:
+            substrings.append(string)
+    return substrings
+
+import pubchempy as pcp
+def smiles(name):
+    smiles_list = []
+    if name == '0' or name == 0:
+        pass
+    else:
+        if ',' not in name:  # Single smiles
+            try:
+                smiles = pcp.get_compounds(name, 'name')[0].isomeric_smiles
+                smiles_list.append(smiles)
+            except:
+                pass
+        else:  # String of molecule names
+            names = name.split(',')
+            merged_names = []
+            temp_name = ""
+            for n in names:
+                if temp_name == "":
+                    temp_name = n
+                elif temp_name.endswith('-') or temp_name.endswith('_'):
+                    temp_name += n
+                else:
+                    merged_names.append(temp_name)
+                    temp_name = n
+            merged_names.append(temp_name)
+
+            for merged_name in merged_names:
+                try:
+                    smiles = pcp.get_compounds(merged_name, 'name')[0].isomeric_smiles
+                    smiles_list.append(smiles)
+                except:
+                    pass
+
+    return '.'.join(smiles_list)
